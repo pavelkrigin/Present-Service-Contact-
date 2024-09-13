ca1d8de42fd5cf6258151959784ad426f9eb4dde
Pavel Krigin <pavel.krigin.85@gmail.com>
2024-09-13 23:29:36 +0300

    fin_commit

--- Present Service Contact ТЗ/Application/SceneDelegate.swift
+++ Present Service Contact ТЗ/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let citiesListVC = CitiesListRouter.createModule()
+        let citiesListVC = CitiesListRouter.createModule() // Создание модуля CitiesList
         let navigationController = UINavigationController(rootViewController: citiesListVC)
         window.rootViewController = navigationController
         self.window = window
